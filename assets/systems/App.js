cba379c770dea917070a339ecc813847075be738
Jefferson <jeffersonferreira.01@pm.me>
2022-07-31 20:29:48 -0300

    🎉 Hello :)

---
+++ assets/systems/App.js
@@ -0,0 +1,12 @@
+﻿const timerLabel = document.querySelector("#timer-value")
+
+const timer = new Timer({
+    onTick: time => timerLabel.innerHTML = Timer.format(time),
+    onStop: () => handleGameTimeout()
+})
+
+const navigation = new NavigationController();
+
+function handleGameTimeout() {
+    console.log("time fucking out...")
+}
---
+++ assets/systems/timer.js
@@ -0,0 +1,37 @@
+﻿class Timer {
+
+    constructor(callbacks) {
+        this.callbacks = callbacks
+    }
+
+    start(durationSec) {
+
+        const id = setInterval(function () {
+            durationSec -= 1;
+            this.callbacks.onTick?.(durationSec)
+
+            if (durationSec <= 0) {
+                clearInterval(id)
+                this.callbacks.onStop?.()
+            }
+        }.bind(this), 1000)
+    }
+
+    static format(time) {
+        const minutes = parseInt(time / 60)
+        const seconds = parseInt(time - (minutes * 60))
+
+        let minString = minutes.toString();
+        let secString = seconds.toString();
+
+        if (minutes < 10) {
+            minString = '0' + minutes
+        }
+
+        if (seconds < 10) {
+            secString = '0' + seconds
+        }
+
+        return `${minString}:${secString}`
+    }
+}
