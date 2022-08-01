f7ee7c3d746289b6dd543557f95610c97a422c9e
Jefferson <jeffersonferreira.01@pm.me>
2022-08-01 07:47:01 -0300

    🚧 Building QuizController

--- assets/systems/App.js
+++ assets/systems/App.js
@@ -10,3 +10,10 @@ const navigation = new NavigationController();
 function handleGameTimeout() {
     console.log("time fucking out...")
 }
+
+navigation.show(1)
+
+
+const quizController = new QuizController()
+
+quizController.promptUser("This is a sample title", ["Option A", "Option B"], 2)
